371b968b82ae81bc948ff008687e826e04c61fab
majatomicmaja <118531291+majatomicmaja@users.noreply.github.com>
2022-11-18 12:12:03 +0100

    Add files via upload
    JavaScript Functions 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// numberToString(Number)
+// Converts a number a string.
+// Parameters
+// n: number
+// Returns: string, the number as a string
+
+let numberToString = 223;
+let logger = numberToString.toString(2);
+console.log(logger);
+
